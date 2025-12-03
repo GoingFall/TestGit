3ccc9bdf9159cf4b207d6ef9577d795f1b22d6f4
GoingFall <13715367571@163.com>
2025-12-03 19:28:03 +0800

    Student1: Add divide function

--- main.c
+++ main.c
@@ -5,9 +5,19 @@ int subtract(int a, int b) {
     return a - b;
 }
 
+// Student1添加了divide函数
+int divide(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    }
+    return 0;
+}
+
 int main() {
     printf("Hello, World!\n");
     int result = subtract(10, 3);
     printf("10 - 3 = %d\n", result);
+    int div_result = divide(20, 4);
+    printf("20 / 4 = %d\n", div_result);
     return 0;
 }
