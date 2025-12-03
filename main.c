89d31aad7cce0cbcce5d355f4dc0baf2834dfd64
GoingFall <13715367571@163.com>
2025-12-03 18:32:53 +0800

    Add multiply function and update main to use both functions

--- main.c
+++ main.c
@@ -5,9 +5,16 @@ int add(int a, int b) {
     return a + b;
 }
 
+// 添加一个乘法函数
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
     printf("Hello, World!\n");
-    int result = add(5, 3);
-    printf("5 + 3 = %d\n", result);
+    int sum = add(5, 3);
+    int product = multiply(4, 6);
+    printf("5 + 3 = %d\n", sum);
+    printf("4 * 6 = %d\n", product);
     return 0;
 }
