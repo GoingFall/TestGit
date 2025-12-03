629c2cb5311a96012d969c25b936da1b4655726c
GoingFall <13715367571@163.com>
2025-12-03 18:32:48 +0800

    Add add function and test it in main

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+// 添加一个计算函数
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    int result = add(5, 3);
+    printf("5 + 3 = %d\n", result);
     return 0;
 }
