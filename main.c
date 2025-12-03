6ffdf7cea40c542e4dd5673ec027eb7407c1d2ed
GoingFall <13715367571@163.com>
2025-12-03 18:33:04 +0800

    Restore main.c to initial version

--- main.c
+++ main.c
@@ -1,20 +1,6 @@
 #include <stdio.h>
 
-// 添加一个计算函数
-int add(int a, int b) {
-    return a + b;
-}
-
-// 添加一个乘法函数
-int multiply(int a, int b) {
-    return a * b;
-}
-
 int main() {
     printf("Hello, World!\n");
-    int sum = add(5, 3);
-    int product = multiply(4, 6);
-    printf("5 + 3 = %d\n", sum);
-    printf("4 * 6 = %d\n", product);
     return 0;
 }
