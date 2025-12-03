d9e26ca882756f25a7df3b6aad849e472c160097
GoingFall <13715367571@163.com>
2025-12-03 18:33:15 +0800

    Add subtract function in feature branch

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+// 在feature分支中添加新功能
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     printf("Hello, World!\n");
+    int result = subtract(10, 3);
+    printf("10 - 3 = %d\n", result);
     return 0;
 }
